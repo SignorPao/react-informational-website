a5d1528f37ec8326189c736eedf075fa6d892d79
Pavel Belousov <signorpao@gmail.com>
2023-01-29 20:32:40 +0300

    ...settings...

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,19 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    container: {
+      padding: {
+        DEFAULT: "16px",
+        xl: "0",
+      },
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1100px",
+      "2xl": "1560px",
+    },
     extend: {},
   },
   plugins: [],
