61143598b561e5e78122bb4b833b452b6bc32735
Pavel Belousov <signorpao@gmail.com>
2023-02-11 10:08:35 +0300

    footer...

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="flex items-center justify-center text-white bg-slate-700 min-h-[500px]">
+    <div className="flex items-center justify-center text-white bg-primary min-h-[500px]">
       Footer
     </div>
   );
--- src/components/HeaderCarousel.jsx
+++ src/components/HeaderCarousel.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useRef} from "react";
 
 // import swiper
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -10,6 +10,13 @@ import "swiper/css/effect-fade";
 // import { headerData } from "../data/data";
 
 const HeaderCarousel = () => {
+  const progressCircle = useRef(null);
+  const progressContent = useRef(null);
+  const onAutoplayTimeLeft = (s, time, progress) => {
+    progressCircle.current.style.setProperty('--progress', 1 - progress);
+    progressContent.current.textContent = `${Math.ceil(time / 1000)}s`;
+  };
+
   return (
     <Swiper
       modules={[Autoplay, EffectFade]}
@@ -20,29 +27,35 @@ const HeaderCarousel = () => {
       autoplay={{
         delay: 6000,
         // disableOnInteraction: true,
-        // pauseOnMouseEnter: false,
+        // pauseOnMouseEnter: true,
       }}
+      onAutoplayTimeLeft={onAutoplayTimeLeft}
       className="w-full h-full"
     >
       <SwiperSlide className="bg-hero1 bg-cover bg-bottom bg-no-repeat w-full h-full">
         <div className="w-full h-full flex items-center justify-center">
-          <h1 className="text-9xl text-accent font-semibold">Интерьер</h1>
+          <h1 className="text-3xl lg:text-6xl xl:text-9xl text-accent font-semibold">Интерьер</h1>
         </div>
       </SwiperSlide>
 
       <SwiperSlide className="bg-hero2 bg-cover bg-bottom bg-no-repeat w-full h-full">
         <div className="w-full h-full flex items-center justify-center">
-          <h1 className="text-9xl text-accent font-semibold">Сад и огород</h1>
+          <h1 className="text-3xl lg:text-6xl xl:text-9xl text-accent font-semibold">Сад и огород</h1>
         </div>
       </SwiperSlide>
-      
+
       <SwiperSlide className="bg-hero3 bg-cover bg-bottom bg-no-repeat w-full h-full">
         <div className="w-full h-full flex items-center justify-center">
-          <h1 className="text-9xl text-accent font-semibold">
-            Стройматериалы
-          </h1>
+          <h1 className="text-3xl lg:text-6xl xl:text-9xl text-accent font-semibold">Стройматериалы</h1>
         </div>
       </SwiperSlide>
+
+      <div className="autoplay-progress" slot="container-end">
+        <svg viewBox="0 0 48 48" ref={progressCircle}>
+          <circle cx="24" cy="24" r="20"></circle>
+        </svg>
+        <span ref={progressContent}></span>
+      </div>
     </Swiper>
   );
 };
